b345585a4c62c1795544349f2c824bdf9588c930
urvesh254 <urveshpatel2001@gmail.com>
2022-04-04 16:14:36 +0530

    Project Initialized.

---
+++ src/main/java/com/itaims/ihs/InvoiceAndHrmsSystemApplication.java
@@ -0,0 +1,13 @@
+package com.itaims.ihs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceAndHrmsSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceAndHrmsSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itaims/ihs/InvoiceAndHrmsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itaims.ihs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceAndHrmsSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
