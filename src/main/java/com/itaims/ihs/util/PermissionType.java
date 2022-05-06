d5c59e43d5604972f4c93197c584930771eb1589
urvesh254 <urveshpatel2001@gmail.com>
2022-05-06 12:41:20 +0530

    Permissions type were added for security purpose.

---
+++ src/main/java/com/itaims/ihs/util/PermissionType.java
@@ -0,0 +1,44 @@
+package com.itaims.ihs.util;
+
+public class PermissionType {
+    public static final String BILL_VIEW = "BILL_VIEW";
+    public static final String BILL_EDIT = "BILL_EDIT";
+    public static final String BILL_DETAIL_VIEW = "BILL_DETAIL_VIEW";
+    public static final String BILL_DETAIL_EDIT = "BILL_DETAIL_EDIT";
+    public static final String BILL_PAYMENT_VIEW = "BILL_PAYMENT_VIEW";
+    public static final String BILL_PAYMENT_EDIT = "BILL_PAYMENT_EDIT";
+    public static final String CURRENCY_VIEW = "CURRENCY_VIEW";
+    public static final String CURRENCY_EDIT = "CURRENCY_EDIT";
+    public static final String CUSTOMER_VIEW = "CUSTOMER_VIEW";
+    public static final String CUSTOMER_EDIT = "CUSTOMER_EDIT";
+    public static final String DEPARTMENT_VIEW = "DEPARTMENT_VIEW";
+    public static final String DEPARTMENT_EDIT = "DEPARTMENT_EDIT";
+    public static final String EMPLOYEE_VIEW = "EMPLOYEE_VIEW";
+    public static final String EMPLOYEE_EDIT = "EMPLOYEE_EDIT";
+    public static final String EXPENSE_VIEW = "EXPENSE_VIEW";
+    public static final String EXPENSE_EDIT = "EXPENSE_EDIT";
+    public static final String INVOICE_VIEW = "INVOICE_VIEW";
+    public static final String INVOICE_EDIT = "INVOICE_EDIT";
+    public static final String INVOICE_DETAIL_VIEW = "INVOICE_DETAIL_VIEW";
+    public static final String INVOICE_DETAIL_EDIT = "INVOICE_DETAIL_EDIT";
+    public static final String INVOICE_PAYMENT_VIEW = "INVOICE_PAYMENT_VIEW";
+    public static final String INVOICE_PAYMENT_EDIT = "INVOICE_PAYMENT_EDIT";
+    public static final String MILESTONE_VIEW = "MILESTONE_VIEW";
+    public static final String MILESTONE_EDIT = "MILESTONE_EDIT";
+    public static final String MILESTONE_MODULE_VIEW = "MILESTONE_MODULE_VIEW";
+    public static final String MILESTONE_MODULE_EDIT = "MILESTONE_MODULE_EDIT";
+    public static final String OPTION_VIEW = "OPTION_VIEW";
+    public static final String OPTION_EDIT = "OPTION_EDIT";
+    public static final String PERMISSION_VIEW = "PERMISSION_VIEW";
+    public static final String PERMISSION_EDIT = "PERMISSION_EDIT";
+    public static final String PROJECT_VIEW = "PROJECT_VIEW";
+    public static final String PROJECT_EDIT = "PROJECT_EDIT";
+    public static final String ROLE_VIEW = "ROLE_VIEW";
+    public static final String ROLE_EDIT = "ROLE_EDIT";
+    public static final String SERVICE_VIEW = "SERVICE_VIEW";
+    public static final String SERVICE_EDIT = "SERVICE_EDIT";
+    public static final String USER_VIEW = "USER_VIEW";
+    public static final String USER_EDIT = "USER_EDIT";
+    public static final String VENDOR_VIEW = "VENDOR_VIEW";
+    public static final String VENDOR_EDIT = "VENDOR_EDIT";
+}
