f4101cc645e651d6694bc0700e0e3380ade0b000
urvesh254 <urveshpatel2001@gmail.com>
2022-05-06 12:44:28 +0530

    Exception handling added for "Access Denied".

--- src/main/java/com/itaims/ihs/error/RestExceptionHandler.java
+++ src/main/java/com/itaims/ihs/error/RestExceptionHandler.java
@@ -3,6 +3,7 @@ package com.itaims.ihs.error;
 import com.itaims.ihs.response.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -38,5 +39,20 @@ public class RestExceptionHandler {
         error.setTimestamp(System.currentTimeMillis());
 
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+    /**
+     * @param exc
+     * @return
+     * @brief Handle access denied exception from rest controller handled here.
+     */
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleAccessDenied(AccessDeniedException exc) {
+
+        ErrorResponse error = new ErrorResponse();
+        error.setStatus(HttpStatus.UNAUTHORIZED.value());
+        error.setMessage(exc.getMessage() == null ? "One of the given object is null" : exc.getMessage());
+        error.setTimestamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.UNAUTHORIZED);
     }
 }
