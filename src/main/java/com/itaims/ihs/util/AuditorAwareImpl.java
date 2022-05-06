78b8e42af11503d285936342539cdac6dc41dff0
urvesh254 <urveshpatel2001@gmail.com>
2022-05-06 12:39:51 +0530

    Created By or Updated by will be updated based on the logged-in user.

--- src/main/java/com/itaims/ihs/util/AuditorAwareImpl.java
+++ src/main/java/com/itaims/ihs/util/AuditorAwareImpl.java
@@ -1,12 +1,16 @@
 package com.itaims.ihs.util;
 
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
+import java.util.logging.Logger;
 
-public class  AuditorAwareImpl implements AuditorAware<String> {
+public class AuditorAwareImpl implements AuditorAware<String> {
+    private static final Logger logger = Logger.getLogger(AuditorAwareImpl.class.getName());
+
     @Override
     public Optional<String> getCurrentAuditor() {
-        return Optional.of("ADMIN");
+        return Optional.of(SecurityContextHolder.getContext().getAuthentication().getName());
     }
 }
